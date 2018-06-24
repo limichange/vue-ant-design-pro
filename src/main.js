c1cf815e07b228d78abb1c396065c4f0de7a454a
limichange <limichange@hotmail.com>
2018-06-24 22:11:02 +0800

    install vue-antd-ui

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vau from 'vue-antd-ui'
+import 'vue-antd-ui/dist/antd.css'
 
 Vue.config.productionTip = false
+
+Vue.use(vau)
 
 new Vue({
   router,
