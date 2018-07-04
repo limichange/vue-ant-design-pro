1de714bec3db92ed29e6806b4014995dcd4e5414
limichange <limichange@hotmail.com>
2018-07-04 21:46:31 +0800

    update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
-<template>
-  <div id="app">
-    <router-view/>
-  </div>
-</template>
+<script>
+export default {
+  name: 'App',
+  render () {
+    return (
+      <div id="app">
+        <router-view />
+      </div>
+    )
+  }
+}
+</script>
+
+<style lang="less">
+#app, body, html {
+  height: 100%;
+}
+</style>
