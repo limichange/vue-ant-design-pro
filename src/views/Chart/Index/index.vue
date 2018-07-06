09e9944899422a09e2b4c10c297cbc1c7cc577ab
limichange <limichange@hotmail.com>
2018-07-06 15:42:12 +0800

    add chart view folder

---
+++ src/views/Chart/Index/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div :class="$style.Index">
+    Index
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<style lang="less" module>
+.Index {}
+</style>
